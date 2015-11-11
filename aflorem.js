05e6688df0c90fd6088929b7ca8a38e359a0034c
AntePerka <anteperkusic990@gmail.com>
2015-11-11 12:22:34 +0100

    fifth

---
+++ aflorem.js
@@ -0,0 +1,8 @@
+function buttonhover() {
+    var x = document.getElementById("button");
+    x.style.opacity = "1";
+}
+function buttonout() {
+	var x = document.getElementById("button");
+	x.style.opacity = "0.8";
+}
